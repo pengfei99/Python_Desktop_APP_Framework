3474a95df54c8d3e7920dba18708a98baceb5c1d
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-11-23 12:31:09 +0100

    add slot improved example

---
+++ src/L08_signal_slot_improved.py
@@ -0,0 +1,49 @@
+import sys
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+from functools import partial
+
+
+# define a slot, it will print message if no message is printed, if there is a message then remove it
+def greet(name):
+    if msgLabel.text():
+        msgLabel.setText("")
+    else:
+        msgLabel.setText(f"Hello, {name}!")
+
+
+# create application
+app = QApplication([])
+
+# define main window
+window = QWidget()
+window.setWindowTitle("Signals and slots")
+
+# setup Vertical layout manager
+layout = QVBoxLayout()
+
+# create a button
+button = QPushButton("Greet")
+# link button clicked signal to slot greet
+button.clicked.connect(partial(greet, "pengfei"))
+
+# add button to layout
+layout.addWidget(button)
+
+# create a text area and add it to layout
+msgLabel = QLabel("")
+layout.addWidget(msgLabel)
+
+# add layout to main window
+window.setLayout(layout)
+
+# show application window
+window.show()
+
+sys.exit(app.exec())
